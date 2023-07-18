e8c6d709107f43b44a10a333ec95daee53ca316e
Zeddy <sirzeddy@gmail.com>
2023-07-18 10:59:34 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return 0;
+putchar('\n');
+return (0);
 }
-
