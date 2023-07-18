3b36f5e26e8104b9841d49078ec72e2dfe1f5794
Zeddy <sirzeddy@gmail.com>
2023-07-18 09:29:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-Here is the C code that prints `_putchar` followed by a new line and returns 0:
-
-```c
-#include <stdio.h>
-
-int main() {
-    printf("_putchar\n");
-    return 0;
-}
-```
-
-When you run this code, it will output `_putchar` followed by a new line. The `return 0` statement indicates that the program has executed successfully.
+#include "main.h" 
+  
+ /** 
+   * main - Entry point 
+   * 
+   * Return: Always 0 
+   */ 
+ int main(void) 
+ { 
+         _putchar('H'); 
+         _putchar('o'); 
+         _putchar('l'); 
+         _putchar('b'); 
+         _putchar('e'); 
+         _putchar('r'); 
+         _putchar('t'); 
+         _putchar('o'); 
+         _putchar('n'); 
+         _putchar('\n'); 
+  
+         return (0); 
+ }
